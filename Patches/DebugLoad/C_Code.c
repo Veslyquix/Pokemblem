c9c7f1a38b02504bc146842b9ec9c8035d5217b0
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-06-30 15:06:50 -0400

    wip debug

--- Patches/DebugLoad/C_Code.c
+++ Patches/DebugLoad/C_Code.c
@@ -1,2 +1,30 @@
 #include "C_Code.h"
 #include "global.h"
+
+extern int CheckIfCaught(int classID);
+extern void AutoLevelSummonedUnit(struct Unit * unit, int levels);
+// extern void RegisterPokemon(int classID);
+// extern struct Unit * FindFreeSlot(void); // returns unit to make
+extern void ChangeS1UnitIntoLowestUnitID(void);
+extern const struct UnitDefinition DefaultUnit[];
+extern int DefaultUnitID_Link;
+
+struct DebugStuffStruct
+{
+};
+extern struct DebugStuffStruct DebugStuff[];
+
+void DebugLoadUnits(void)
+{
+    int classID;
+    int level;
+    struct Unit * unit;
+    gEventSlots[1] = DefaultUnitID_Link; // unit ID
+    gEventSlots[3] = 1;                  // visible levels
+    if (!CheckIfCaught(classID) {
+        unit = LoadUnit(DefaultUnit);
+        unit->pClassData = classTablePoin[classID];
+        AutoLevelSummonedUnit(unit, level);
+        ChangeS1UnitIntoLowestUnitID(); 
+    }
+}
