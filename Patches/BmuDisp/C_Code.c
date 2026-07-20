c8a49d957815484e694afcb964c9c0c642b5de29
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-07-20 14:08:18 -0400

    fix the last row in prep item menu from showing an extra name

--- Patches/BmuDisp/C_Code.c
+++ Patches/BmuDisp/C_Code.c
@@ -60,6 +60,78 @@ extern u16 SmsObjVramUpperChr;
 // {
 // return gMUGfxBuffer + (sMuImgBufOffLut[slot] * MU_GFX_MAX_SIZE);
 // }
+// sub_809A114 809872A 809a174
+void sub_809A114(
+    struct PrepItemScreenProc * proc, u8 row, s8 flag) // fix the last row in prep item menu from showing an extra name
+{
+    int i;
+    int idx;
+    struct Text * text;
+    bool isWmSecretShop;
+
+    idx = row * 3;
+    text = &gPrepItemTexts[idx % 15];
+
+    if (gGMData.state.bits.state_0 && GetGMapBaseMenuKind() == SHOP_TYPE_SECRET_SHOP)
+    {
+        isWmSecretShop = true;
+    }
+    else
+    {
+        isWmSecretShop = false;
+    }
+
+    for (i = 0; i < 3; text++, i++)
+    {
+        int x;
+        int y;
+
+        if (flag == 0)
+        {
+            ClearText(text);
+        }
+        x = (i % 3) * 8;
+        y = (row * 2) & 31;
+        if (idx + i >= PrepGetUnitAmount())
+        {
+            // fix the last row in prep item menu from showing an extra name
+            // I dunno why this is necessary, but it fixes the issue.
+            PutBlankText(text, TILEMAP_LOCATED(gBG2TilemapBuffer, x, y));
+            continue;
+        }
+
+        if (flag == 0)
+        {
+            struct Unit * unit = GetUnitFromPrepList(idx + i);
+
+            Text_SetCursor(text, 0);
+
+            if (isWmSecretShop)
+            {
+                if (UnitHasItem(unit, ITEM_MEMBERCARD))
+                {
+                    Text_SetColor(text, TEXT_COLOR_SYSTEM_WHITE);
+                }
+                else
+                {
+                    Text_SetColor(text, TEXT_COLOR_SYSTEM_GRAY);
+                }
+            }
+            else
+            {
+                Text_SetColor(text, TEXT_COLOR_SYSTEM_WHITE);
+            }
+
+            Text_DrawString(text, GetStringFromIndex(unit->pClassData->nameTextId));
+        }
+
+        PutText(text, TILEMAP_LOCATED(gBG2TilemapBuffer, x, y));
+    }
+
+    BG_EnableSyncByMask(BG2_SYNC_BIT);
+
+    return;
+}
 
 extern struct TalkState * sTalkState;
 void HookFace_Shop_Init(int fid)
