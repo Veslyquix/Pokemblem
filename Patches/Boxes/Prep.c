58a1962cc0a3acc8f5c56c42119c04b08bffdd86
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-07-20 14:52:41 -0400

    fix SMS in prep changing during fadeout

--- Patches/Boxes/Prep.c
+++ Patches/Boxes/Prep.c
@@ -1310,7 +1310,7 @@ void ProcPrepUnit_Idle(struct ProcPrepUnit * proc)
 
         if (B_BUTTON & gKeyStatusPtr->newKeys)
         {
-            CallDeploySelectedUnits();
+            // CallDeploySelectedUnits(); // moved into Prep.h's ProcCmd instructions to be after the fade out
             PlaySoundEffect(0x6B);
             Proc_Goto(proc, PROC_LABEL_PREPUNIT_PRESS_B);
             return;
