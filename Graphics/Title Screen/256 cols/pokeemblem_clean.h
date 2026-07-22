862fce0e571d6f523b3e117959ed8f2f9de0edb8
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-07-22 10:30:17 -0400

    title screen nonsense

---
+++ Graphics/Title Screen/256 cols/pokeemblem_clean.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(pokeemblem_clean)
+
+//======================================================================
+//
+//	pokeemblem_clean, 240x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ 600 tiles not compressed
+//	Total size: 512 + 38400 = 38912
+//
+//	Time-stamp: 2026-07-22, 10:21:16
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_POKEEMBLEM_CLEAN_H
+#define GRIT_POKEEMBLEM_CLEAN_H
+
+#define pokeemblem_cleanTilesLen 38400
+extern const unsigned short pokeemblem_cleanTiles[19200];
+
+#define pokeemblem_cleanPalLen 512
+extern const unsigned short pokeemblem_cleanPal[256];
+
+#endif // GRIT_POKEEMBLEM_CLEAN_H
+
+//}}BLOCK(pokeemblem_clean)
