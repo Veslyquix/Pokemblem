8396526535dc1c9ea61c717f7f481978a1d51dd6
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-07-30 13:38:46 -0400

    make chibis disappear with endface

--- Patches/BugFixes/VanillaRoutineFixes.c
+++ Patches/BugFixes/VanillaRoutineFixes.c
@@ -1,68 +1,75 @@
 
 #include "gbafe.h"
 
-
-
-
-extern struct Anim* sFirstAnim;
+extern struct Anim * sFirstAnim;
 
 extern struct MsgBuffer sMsgString;
 
-
 //! FE8U = 0x08005738
-void EndFace(struct FaceProc* proc) {
-	if (proc) {
-    gFaces[proc->faceSlot] = NULL;
-    Proc_End(proc);
-	} 
+void EndFace(struct FaceProc * proc)
+{
+    EndFaceChibiSpr(); // 8005B68
+    if (proc)
+    {
+        gFaces[proc->faceSlot] = NULL;
+        Proc_End(proc);
+    }
 
     return;
 }
 
 //! FE8U = 0x08005758
-void EndFaceById(int slot) {
-	if (gFaces[slot]) { 
-    EndFace(gFaces[slot]);
-	} 
+void EndFaceById(int slot)
+{
+    EndFaceChibiSpr();
+    if (gFaces[slot])
+    {
+        EndFace(gFaces[slot]);
+    }
     return;
 }
 
 //! FE8U = 0x080057A4
-int GetFaceDisplayBits(struct FaceProc* proc) {
-	if (proc) return proc->displayBits;
-	return 0; 
+int GetFaceDisplayBits(struct FaceProc * proc)
+{
+    if (proc)
+        return proc->displayBits;
+    return 0;
 }
 
-void AnimDelete(struct Anim* anim)
+void AnimDelete(struct Anim * anim)
 {
-	if (anim) { 
-    if (anim->pPrev == NULL)
+    if (anim)
     {
-        sFirstAnim = anim->pNext;
-        anim->pNext->pPrev = NULL;
-    }
-    else
-    {
-        anim->pPrev->pNext = anim->pNext;
-        anim->pNext->pPrev = anim->pPrev;
-    }
+        if (anim->pPrev == NULL)
+        {
+            sFirstAnim = anim->pNext;
+            anim->pNext->pPrev = NULL;
+        }
+        else
+        {
+            anim->pPrev->pNext = anim->pNext;
+            anim->pNext->pPrev = anim->pPrev;
+        }
 
-    anim->state = 0;
-    anim->pPrev = NULL;
-    anim->pNext = NULL;
-	} 
+        anim->state = 0;
+        anim->pPrev = NULL;
+        anim->pNext = NULL;
+    }
 }
 
 extern struct ProcCmd CONST_DATA ProcScr_LightRuneAnim[];
 
 void StartLightRuneAnim(ProcPtr parent, int x, int y)
 {
-	if (parent) { 
-    Proc_StartBlocking(ProcScr_LightRuneAnim, parent);
-	} 
-	else {
-		Proc_Start(ProcScr_LightRuneAnim, (ProcPtr)3);
-	}
+    if (parent)
+    {
+        Proc_StartBlocking(ProcScr_LightRuneAnim, parent);
+    }
+    else
+    {
+        Proc_Start(ProcScr_LightRuneAnim, (ProcPtr)3);
+    }
 
     x = x * 0x10 - gBmSt.camera.x - 0x18;
     y = y * 0x10 - gBmSt.camera.y - 0x28;
@@ -82,7 +89,8 @@ char * StrInsertTact(void)
     while ((r0 = *r5))
     {
         r1 = r0;
-        while (0) ;
+        while (0)
+            ;
         if (r1 < 0x20)
         {
             *r4 = r0;
@@ -102,9 +110,9 @@ char * StrInsertTact(void)
             if (*r5 != 0x20)
             {
                 *r4++ = r1;
-				if (*r5 == 0x0) break; // added for UTF8 compatibility 
+                if (*r5 == 0x0)
+                    break; // added for UTF8 compatibility
                 *r4++ = *r5++;
-				
             }
             else
             {
@@ -119,4 +127,3 @@ char * StrInsertTact(void)
     *r4 = 0;
     return sMsgString.buffer5;
 }
-
